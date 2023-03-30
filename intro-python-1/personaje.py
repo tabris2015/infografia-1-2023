e0ce379807192ad1520eb5ff0ed1bd0e9d2ded82
Jose Laruta <eduardo.laruta@gmail.com>
2023-03-30 09:41:37 -0400

    clases

---
+++ intro-python-1/personaje.py
@@ -0,0 +1,48 @@
+import time
+class Personaje:
+    def __init__(self, nombre, vitalidad):
+        self.nombre = nombre
+        self.vitalidad = vitalidad
+    
+    def saludo(self):
+        print(f"Hola, mi nombre es {self.nombre}")
+
+    def esta_vivo(self):
+        return self.vitalidad > 0
+
+class Jugador(Personaje):
+    def __init__(self, nombre, vitalidad, habilidades):
+        super().__init__(nombre, vitalidad)
+        self.habilidades = habilidades
+    
+    def recibir_daño(self, daño):
+        self.vitalidad -= daño
+
+    def listar_habilidades(self):
+        for h in self.habilidades:
+            print(f"Puedo {h}")
+
+class Enemigo(Personaje):
+    def __init__(self, nombre, vitalidad, daño, ataque_esp):
+        super().__init__(nombre, vitalidad)
+        self.daño = daño
+        self.ataque_esp = ataque_esp
+    
+    def atacar_jugador(self, jugador):
+        print(f"Enemigo {self.nombre} atacando a jugador {jugador.nombre} con daño: {self.daño}")
+        jugador.recibir_daño(self.daño)
+
+
+jugador = Jugador("Juan", 100, ["atacar", "volar", "esquivar"])
+jugador.listar_habilidades()
+jugador.saludo()
+
+enemigo = Enemigo("Raul", 50, 10, 70)
+
+while jugador.esta_vivo():
+    enemigo.atacar_jugador(jugador)
+    print(f"vitalidad {jugador.nombre}: {jugador.vitalidad}")
+    time.sleep(2)
+
+print(f"El jugador {jugador.nombre} ha muerto")
+
