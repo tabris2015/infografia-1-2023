08f6cd0a5bd9fdd5012ecf53edfcddc2cebcad5a
Jose Laruta <eduardo.laruta@gmail.com>
2023-04-04 08:41:03 -0400

    base para deteccion de colision.

--- game_loop/main.py
+++ game_loop/main.py
@@ -13,6 +13,7 @@ class App(arcade.Window):
         self.px = SCREEN_WIDTH / 2
         self.py = SCREEN_HEIGHT / 2 # comienza en el centro de la pantalla
         self.movement = 5
+        self.points = []
 
     def on_key_press(self, symbol: int, modifiers: int):
         """Metodo para detectar teclas que han sido presionada
@@ -34,17 +35,46 @@ class App(arcade.Window):
             print("derecha")
             self.px += self.movement
 
-
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
-        """Metodo para detectar clics del mouse"""
+        """Metodo para detectar clics del mouse
+        En la posicion del clic, se agregara un nuevo punto
+        Argumentos:
+            x: coordenada x del clic
+            y: coordenada y del clic
+            button: boton del mouse presionado
+            modifiers: teclas modificadoras presionadas (shift, ctrl, etc)
+        """
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            print(f"Agregando punto ({x}, {y})")
+            self.points.append((x, y))
 
     def on_update(self, delta_time: float):
         """Metodo para actualizar objetos de la app"""
+        print(f"{1/delta_time}")
+
+    def player_is_on_food(self):
+        """Funcion que detecta si el jugador ha colisionado
+        con un punto.
+        Devuelve el indice del punto en la lista self.points si 
+        existe una colision.
+        Si no existe la colision, devuelve el valor de -1.
+        Ejemplo:
+        self.points = [
+        0:    (3, 4),
+        1:    (6, 7),
+        2:    (18, 20)
+            ]
+        jugador colisiona con punto en (6, 7): retornar 1
+        jugador no colisiona con ningun punto: retornar -1
+        """
+        return -1
 
     def on_draw(self):
         """Metodo para dibujar en la pantalla"""
         arcade.start_render()
-        arcade.draw_point(self.px, self.py, arcade.color.RED, 5)
+        arcade.draw_point(self.px, self.py, arcade.color.RED, 10)
+        if self.points:
+            arcade.draw_points(self.points, arcade.color.GREEN, 5)
 
 
 if __name__ == "__main__":
